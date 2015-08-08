142c9930f90762a3c64947d8c63b9092c8a969b5
Kalaksi <kalaksi@users.noreply.github.com>
2015-08-08 18:55:47 +0300

    Move license inside source file + encoding header

--- lookup/fwdns.py
+++ lookup/fwdns.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Original purpose of this ansible lookup plugin is to help to generate variables for 
-# Shorewall firewall by resolving domain addresses. 
-# Other modules that I found lacked features such as support for IPv6 and creating 
-# network addresses for subnets. 
+# Copyright (c) 2015 kalaksi@users.noreply.github.com
 # 
-# Requires netaddr-module for address transformations.
-
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import socket
 import netaddr
@@ -14,6 +28,12 @@ from ansible import utils, errors
 
 class LookupModule(object):
     '''
+    Original purpose of this ansible lookup plugin is to help to generate variables for 
+    Shorewall firewall by resolving domain addresses. 
+    Other modules that I found lacked features such as support for IPv6 and creating 
+    network addresses for subnets. 
+    Requires netaddr-module for address transformations.
+
     Examples of supported parameter formats:
     'example.com'                       # Basic domain name
     'example.com/24'                    # Include netmask bits, network address is returned in CIDR
